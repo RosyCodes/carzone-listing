5ad0e8c68afe92a64735f8067f7f6f73c8fd3b3b
Rosilie Lim <rosilielim@gmail.com>
2024-07-10 15:28:17 -0500

    first commit

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Rosilie\OneDrive\Desktop\LEARNING DJANGO PROJECTS\Carzone-Project\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
