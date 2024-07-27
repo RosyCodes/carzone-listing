95a5b14420792dcedbe582fdb052d113358affe8
Rosilie Lim <rosilielim@gmail.com>
2024-07-27 13:40:24 -0500

    Updated whitenoise static file storage

--- carzone/settings.py
+++ carzone/settings.py
@@ -190,5 +190,5 @@ EMAIL_HOST_PASSWORD = 'xmkamjunbsjvvipt'
 EMAIL_USE_TLS = True
 
 # Whitenoise Configuration for HEROKU deployment
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 django_heroku.settings(locals())
